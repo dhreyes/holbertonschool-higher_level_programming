e97b18891ad97193fa1cfd6e6a6c14734dd807f7
dhreyes <2590@holbertonschool.com>
2021-04-26 19:19:17 +0000

    Answer task 10 using hop method

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * check_cycle - checks linked list for a loop
+ * @list: head of linked list
+ * Return: 0 if no loop, else 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *step = list, *jump = list;
+
+	while (jump)
+	{
+		step = step->next;
+		jump = (jump->next)->next;
+		if (step == jump)
+			return (1);
+	}
+	return (0);
+}
