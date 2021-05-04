d3cff228fe35e792eb4c9fcda28be88312d62648
dhreyes <2590@holbertonschool.com>
2021-05-04 16:30:06 +0000

    Get rid of endspaces on each line

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for idx in range(0, len(matrix)):
         for jdx in range(0, len(matrix[idx])):
             print("{:d}".format(matrix[idx][jdx]), end="")
-            if (jdx <= len(matrix[idx])):
+            if (jdx <= len(matrix[idx]) - 2):
                 print(" ", end="")
         print("\n", end="")
