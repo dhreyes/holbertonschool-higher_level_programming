2c6962f4129cb4b6ce93d4da14ee95418a473dbf
dhreyes <2590@holbertonschool.com>
2021-05-04 16:27:06 +0000

    Conditional for spaces added

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for idx in range(0, len(matrix)):
         for jdx in range(0, len(matrix[idx])):
-            print("{:d} ".format(matrix[idx][jdx]), end="")
+            print("{:d}".format(matrix[idx][jdx]), end="")
+            if (jdx <= len(matrix[idx])):
+                print(" ", end="")
         print("")
