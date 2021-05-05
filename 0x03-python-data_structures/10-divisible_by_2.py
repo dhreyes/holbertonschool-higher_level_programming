02b9c0f83e29f862f399ba252453ac154f2c12e0
dhreyes <2590@holbertonschool.com>
2021-05-05 02:04:37 +0000

    No more range

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = []
-    for idx in range(0, len(my_list) + 1):
+    for idx in my_list:
         if idx % 2 == 0:
             new_list.append(True)
         else:
