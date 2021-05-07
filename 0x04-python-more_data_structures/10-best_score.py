edbde185db95bba1f2f446d73e42b774a490e2ec
dhreyes <2590@holbertonschool.com>
2021-05-07 12:18:44 +0000

    None edge case add for value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,5 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     value = list(a_dictionary.values())
+    if len(value) == 0:
+        return None
     key = list(a_dictionary.keys())
     return key[value.index(max(value))]
