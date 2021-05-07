0c7988c649e6b7587774e8fa44dc59e2efdbc964
dhreyes <2590@holbertonschool.com>
2021-05-07 01:35:11 +0000

    Gusty so smart

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
-        return 0
-    if roman_string is not str:
+    if roman_string is None or type(roman_string) is not str:
         return 0
     d = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
     ans = 0
