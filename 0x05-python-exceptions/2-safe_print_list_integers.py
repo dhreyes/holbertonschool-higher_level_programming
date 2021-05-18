91a625278991366ffbf93c02865559af1462201d
dhreyes <2590@holbertonschool.com>
2021-05-18 00:25:15 +0000

    Answer task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    idx = 0
+    count = 0
+    while idx < x:
+        try:
+            print("{:d}".format(my_list[idx]), end="")
+            idx += 1
+            count += 1
+        except IndexError as err:
+            raise err
+        except:
+            idx += 1
+    print("")
+    return count
