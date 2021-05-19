373270b8858b2b0c37253bc843a7a9922a45ec9b
dhreyes <2590@holbertonschool.com>
2021-05-19 00:30:35 +0000

    Answer task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Continuing to define class Square in task 1 """
+
+
+class Square:
+    """ A class for a Square """
+
+    def __init__(self, size):
+        """ Initializes an instance of the class Square takes the size """
+
+        """ size is the new size for the size portion of Square """
+
+        self.__size = size
