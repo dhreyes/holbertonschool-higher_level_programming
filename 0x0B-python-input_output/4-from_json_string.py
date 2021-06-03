51d46d840a7330786befeed4e6609b60b5480f47
dhreyes <2590@holbertonschool.com>
2021-06-03 23:17:37 +0000

    Answer task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Write a function that returns an object (Python data structure)
+represented by a JSON string:
+
+    Prototype: def from_json_string(my_str):
+    You don’t need to manage exceptions if the
+    JSON string doesn’t represent an object.
+"""
+
+
+def from_json_string(my_str):
+    """ returns object from json string """
+    import json
+
+    return json.loads(my_str)
