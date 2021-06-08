e7a9fcd91a2541e0ebc1501f174ae7432166e467
dhreyes <2590@holbertonschool.com>
2021-06-08 03:16:44 +0000

    Test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,18 +4,64 @@ import unittest
 from models.base import Base
 import pep8
 
+
 class TestBase(unittest.TestCase):
     """ tests stuff in unittest standard """
 
     def test_pep8_conformance(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['file1.py', 'file2.py'])
+        result = pep8style.check_files(['models/base.py',
+                                        'tests/test_models/test_base.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def setUp(self):
-        pass
+        """ Sets up the class for testing """
+        self.b0 = Base(0)
+        self.b1 = Base(100)
+        self.b2 = Base(10)
+        self.b3 = Base(-13.2)
+        self.b4 = Base("string")
+        self.b5 = Base([1, 2, 3])
+        self.b6 = Base({2: "hello", 4: "world"})
+        self.b7 = Base()
+        self.b8 = Base()
+        self.b9 = Base()
+        print("I'm running first")
+
+    def test_empty_constructors(self):
+        """ tests if counter in base works correctly """
+        self.assertEqual(self.b7.id, 1)
+        self.assertEqual(self.b8.id, 2)
+        self.assertEqual(self.b9.id, 3)
+
+    def test_ids_as_input(self):
+        """ tests the input for id """
+        self.assertEqual(self.b0.id, 0)
+        self.assertEqual(self.b1.id, 100)
+        self.assertEqual(self.b2.id, 10)
+        self.assertEqual(self.b3.id, -13.2)
+        self.assertEqual(self.b4.id, "string")
+        self.assertEqual(self.b5.id, [1, 2, 3])
+        self.assertEqual(self.b6.id, {2: "hello", 4: "world"})
+
+    def test_to_json_string(self):
+        """ testing to_json_string method """
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(Base.to_json_string([]), "[]")
+        self.assertEqual(Base.to_json_string([{'json': 69}, {'file': 96}]),
+                         '[{"json": 69}, {"file": 96}]')
+        self.assertEqual(type(Base.to_json_string([{'json': 69},
+                                                   {'file': 96}])), str)
+
+    def test_from_json_string(self):
+        self.assertEqual(Base.from_json_string(None), '[]')
+        self.assertEqual(Base.from_json_string("[]"), [])
+        self.assertEqual(Base.from_json_string('[{"json": 69}, {"file": 96}]'),
+                         [{"json": 69}, {"file": 96}])
+        self.assertEqual(type(Base.from_json_string('[{"json": 69},\
+                                                   {"file": 96}]')), list)
 
     def tearDown(self):
-        pass
+        print("teehee hee")
