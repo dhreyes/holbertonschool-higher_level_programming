c375fdd7853151a6932f4e28cb02a7d6df965a42
dhreyes <2590@holbertonschool.com>
2021-07-13 15:14:50 -0400

    Add name

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,5 @@
 -- script list all records with score above 10 in second_table
 -- List score >= 10
-SELECT score FROM second_table
+SELECT score, name FROM second_table
 WHERE score >= 10
 ORDER BY score DESC;
