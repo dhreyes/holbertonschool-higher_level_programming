a40291c177b29f7ba1126d4c3025dfd734127e7a
dhreyes <2590@holbertonschool.com>
2021-07-14 21:15:32 -0400

    ANSWER TASK 3

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- Script to create table force_name with id and name
+-- Create force_name with id and name, name cannot be null
+CREATE TABLE IF NOT EXISTS force_name(
+	id INT,
+	name VARCHAR9(256) NOT NULL);
