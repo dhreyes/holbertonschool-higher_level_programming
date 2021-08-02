71dc4ca9679b5e097f5f7fc26527f8137b810eb5
dhreyes <2590@holbertonschool.com>
2021-08-02 14:51:47 -0400

    Answer task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const myArgs = process.argv.slice(2);
+if (myArgs.length === 0) { console.log('No Argument'); } else if (myArgs.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
