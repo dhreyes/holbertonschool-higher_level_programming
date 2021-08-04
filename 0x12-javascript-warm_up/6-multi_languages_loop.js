850412bb3e389887694bc6bcf71c50a3f8fac1fd
dhreyes <2590@holbertonschool.com>
2021-08-04 19:23:42 -0400

    Add task 8 and change 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+array.forEach((element) => {
+  console.log(element);
+});
---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+let square = '';
+const myArgs = parseInt(process.argv[2]);
+if (isNaN(myArgs)) {
+  console.log('Missing size');
+} else {
+  for (i = 0; i < myArgs; i++) {
+    square = square.concat('X');
+  }
+  for (j = 0; j < myArgs; j++) {
+    console.log(square);
+  }
+}
