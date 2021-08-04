019055a8836621c4c6a98ddd4907d174736da141
dhreyes <2590@holbertonschool.com>
2021-08-04 19:33:00 -0400

    some change

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 let square = '';
-let i, j;
 const myArgs = parseInt(process.argv[2]);
 if (isNaN(myArgs)) {
   console.log('Missing size');
 } else {
-  for (i = 0; i < myArgs; i++) {
+  for (let i = 0; i < myArgs; i++) {
     square = square.concat('X');
   }
-  for (j = 0; j < myArgs; j++) {
+  for (let j = 0; j < myArgs; j++) {
     console.log(square);
   }
 }
