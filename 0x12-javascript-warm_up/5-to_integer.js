21d6919c99ab4d4f6c3f98b62e60eb11093c681d
dhreyes <2590@holbertonschool.com>
2021-08-04 11:52:23 -0400

    Semistandard fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const myArgs = parseInt(process.argv[2]);
 if (isNaN(myArgs)) {
-console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log('My number: ' + myArgs);
+  console.log('My number: ' + myArgs);
 }
