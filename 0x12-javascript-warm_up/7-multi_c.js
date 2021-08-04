49ec68523258bb766f17ac475b542f1ac24450ef
dhreyes <2590@holbertonschool.com>
2021-08-04 14:18:42 -0400

    Answer task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+let myArgs = parseInt(process.argv[2]);
+if (isNaN(myArgs)) {
+  console.log('Missing number of occurences');
+} else {
+  while (myArgs > 0) {
+    console.log('C is fun');
+    myArgs--;
+  }
+}
