6f79399d09d9f54215e6dcdd6f2a2070966eae8f
dhreyes <2590@holbertonschool.com>
2021-09-03 02:14:04 +0000

    Fix everything

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # take in URL as argument, display body of response, variable must be sent with value 98
-curl -sI "$1" -H "X-HolbertonSchool-User-Id: 98"
+curl -s --header "X-HolbertonSchool-User-Id: 98" $1
