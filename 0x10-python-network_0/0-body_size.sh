f0101294bb2f457fc498fb4c82aa77ae3103b32c
dhreyes <2590@holbertonschool.com>
2021-09-03 01:35:00 +0000

    Add limiter

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, send request to URL, display size of body to response
-curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f
+curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f 2
