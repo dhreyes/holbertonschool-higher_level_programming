4047ef908b38891787cde5e450833aaad90b2901
dhreyes <2590@holbertonschool.com>
2021-09-03 02:52:31 +0000

    Answer task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Fetch https://intranet.hbtn.io/status"""
+import urllib.request
+
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    o = "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+    print(o.format(type(html), html, html.decode('utf8')))
