c1357f1c7cca4936b15bd82850b694edfd4a636b
dhreyes <2590@holbertonschool.com>
2021-12-13 22:21:33 -0400

    Answer task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+
+function add(a, b) {
+  return a + b;
+}
+
+const args = process.argv.slice(2, 4);
+args[0] = parseInt(args[0]); // convert string to int
+args[1] = parseInt(args[1]); // convert string to int
+
+console.log(add(args[0], args[1]));
