9afe1afaf6e313128de7b03fb95105f5438613e1
dhreyes <2590@holbertonschool.com>
2021-12-15 03:27:42 +0000

    Semistandard fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
-      if (h > 0 && w > 0) {
-          this.height = h;
-          this.width = w;
-      }
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.height = h;
+      this.width = w;
     }
+  }
 
-// Print rectangle using the character 'X'
-print () {
+  // Print rectangle using the character 'X'
+  print () {
     for (let i = this.height; i--;) {
-        console.log("X".repeat(this.width));
+      console.log('X'.repeat(this.width));
     }
-    }
+  }
 }
 
 module.exports = Rectangle;
