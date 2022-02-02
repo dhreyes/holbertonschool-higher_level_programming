73c435298141bc6c15719d6362e0690edd13c9dc
dhreyes <2590@holbertonschool.com>
2022-02-01 21:15:08 -0400

    Agaiiiin

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
-request(process.argv[2], (err, response) {
+request(process.argv[2], function (err, response) {
   if (err) {
     console.log(err);
   } else {
