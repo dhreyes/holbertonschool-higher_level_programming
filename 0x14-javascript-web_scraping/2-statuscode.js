37d0dd715915cbb5145b2ef5c41e91df699c37b4
dhreyes <2590@holbertonschool.com>
2022-02-01 21:13:24 -0400

    Tryna fix this bs

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
-request(process.argv[2], (err, response, body) => {
+request(process.argv[2], (err, response) {
   if (err) {
     console.log(err);
   } else {
