e7d380ba960f2cc1500b39f42ab5d0b88db384cb
dhreyes <2590@holbertonschool.com>
2022-02-01 20:36:48 -0400

    First iteration of task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+// Write a script that writes a string to a file
+// The script should take a string and a file name as arguments
+// The script should write the string to the file
+
+const fs = require('fs');
+const fileName = process.argv[2];
+const text = process.argv[3];
+
+fs.writeFile(fileName, text, (err) => {
+    if (err) {
+        console.log(err);
+    }
+    });
