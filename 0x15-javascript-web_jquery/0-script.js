f9f2a2e528de8716bcc3dee6ee8508d6df1ad80e
dhreyes <2590@holbertonschool.com>
2022-02-09 19:32:26 -0400

    Add task 0

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+// Write a Javascript script that updates the text color of the <header> element to red (#FF0000)
+// Must use document.querySelector() to select the HTML tag
+// Cant use the JQUERY API
+document.querySelector('header').style.color = '#FF0000';
